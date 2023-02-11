a7d3b16dc40e9566d5868548770968b6a2ac92e7
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 16:02:16 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/products.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/products.java
@@ -16,4 +16,52 @@ public class products {
         this.product_cat = product_cat;
         this.product_price = product_price;
     }
+
+    public int getProduct_ID() {
+        return product_ID;
+    }
+
+    public void setProduct_ID(int product_ID) {
+        this.product_ID = product_ID;
+    }
+
+    public String getProduct_title() {
+        return product_title;
+    }
+
+    public void setProduct_title(String product_title) {
+        this.product_title = product_title;
+    }
+
+    public String getProduct_imgs() {
+        return product_imgs;
+    }
+
+    public void setProduct_imgs(String product_imgs) {
+        this.product_imgs = product_imgs;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+
+    public String getProduct_cat() {
+        return product_cat;
+    }
+
+    public void setProduct_cat(String product_cat) {
+        this.product_cat = product_cat;
+    }
+
+    public int getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(int product_price) {
+        this.product_price = product_price;
+    }
 }
