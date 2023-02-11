87ea4cd7a9323f972640adcd028b26d2a849e263
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 15:36:31 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/db/MyConnection.java
+++ KingsMen/src/main/java/com/example/KingsMen/db/MyConnection.java
@@ -8,6 +8,14 @@ public class MyConnection {
     static Connection connection = null;
 
     public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:1433/youtubelive","root","Zafar@2020");
+            System.out.println("Connection Established");
+        }catch (ClassNotFoundException | SQLException ex){
+            ex.printStackTrace();
+        }
+        return connection;
 
     }
 }
