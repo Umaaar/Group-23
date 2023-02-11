b847d334facf395517dc10c2afac958164900d2d
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 15:41:15 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
@@ -7,4 +7,15 @@ public class accounts {
     private String account_email;
     private String account_password;
     private String account_address;
+    private String account_type;
+
+    public accounts(int account_id, String account_fname, String account_lname, String account_email, String account_password, String account_address, String account_type) {
+        this.account_id = account_id;
+        this.account_fname = account_fname;
+        this.account_lname = account_lname;
+        this.account_email = account_email;
+        this.account_password = account_password;
+        this.account_address = account_address;
+        this.account_type = account_type;
+    }
 }
