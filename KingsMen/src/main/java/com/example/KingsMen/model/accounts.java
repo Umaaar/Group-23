d8c2fe25efc998332c34fa86d035a3a89ab3c810
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 15:38:22 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
@@ -1,4 +1,10 @@
 package com.example.KingsMen.model;
 
 public class accounts {
+    private int account_id;
+    private String account_fname;
+    private String account_lname;
+    private String account_email;
+    private String account_password;
+    private String account_address;
 }
