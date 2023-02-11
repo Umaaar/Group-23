290613e3dde09e37446dad10a49455bf12795dc5
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 15:42:26 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/accounts.java
@@ -18,4 +18,61 @@ public class accounts {
         this.account_address = account_address;
         this.account_type = account_type;
     }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getAccount_fname() {
+        return account_fname;
+    }
+
+    public void setAccount_fname(String account_fname) {
+    }
+
+    public String getAccount_lname() {
+        return account_lname;
+    }
+
+    public void setAccount_lname(String account_lname) {
+        this.account_lname = account_lname;
+    }
+
+    public String getAccount_email() {
+        return account_email;
+    }
+
+    public void setAccount_email(String account_email) {
+        this.account_email = account_email;
+    }
+
+    public String getAccount_password() {
+        return account_password;
+    }
+
+    public void setAccount_password(String account_password) {
+        this.account_password = account_password;
+    }
+
+    public String getAccount_address() {
+        return account_address;
+    }
+
+    public void setAccount_address(String account_address) {
+        this.account_address = account_address;
+    }
+
+    public String getAccount_type() {
+        return account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+
+
 }
