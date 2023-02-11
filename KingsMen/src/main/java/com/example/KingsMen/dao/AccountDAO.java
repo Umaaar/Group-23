248b44965178eefd5ba2ca8c9d077ca1859c41af
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 16:06:14 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/dao/AccountDAO.java
+++ KingsMen/src/main/java/com/example/KingsMen/dao/AccountDAO.java
@@ -1,4 +1,21 @@
 package com.example.KingsMen.dao;
 
+import com.example.KingsMen.db.MyConnection;
+import com.example.KingsMen.model.accounts;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountDAO {
+    public List<accounts> getAllAccounts() throws SQLException {
+        List<accounts> accounts = new ArrayList<>();
+        Connection conn = MyConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement("select * from user");
+
+        return accounts;
+    }
+
 }
