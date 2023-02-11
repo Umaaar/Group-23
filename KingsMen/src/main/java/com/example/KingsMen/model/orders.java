3c7a4c835602077651e881f7097c306b6d809dee
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 15:57:14 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/orders.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/orders.java
@@ -16,4 +16,52 @@ public class orders {
         this.order_status = order_status;
         this.order_total = order_total;
     }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getOrder_name() {
+        return order_name;
+    }
+
+    public void setOrder_name(String order_name) {
+        this.order_name = order_name;
+    }
+
+    public String getOrder_email() {
+        return order_email;
+    }
+
+    public void setOrder_email(String order_email) {
+        this.order_email = order_email;
+    }
+
+    public int getOrder_product_id() {
+        return order_product_id;
+    }
+
+    public void setOrder_product_id(int order_product_id) {
+        this.order_product_id = order_product_id;
+    }
+
+    public String getOrder_status() {
+        return order_status;
+    }
+
+    public void setOrder_status(String order_status) {
+        this.order_status = order_status;
+    }
+
+    public int getOrder_total() {
+        return order_total;
+    }
+
+    public void setOrder_total(int order_total) {
+        this.order_total = order_total;
+    }
 }
