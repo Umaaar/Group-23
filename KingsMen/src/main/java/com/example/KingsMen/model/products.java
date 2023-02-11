361d73457296f66dcf671ea4da0849220c3a4462
farandead <92890148+farandead@users.noreply.github.com>
2023-02-11 16:01:53 +0000

    database

--- KingsMen/src/main/java/com/example/KingsMen/model/products.java
+++ KingsMen/src/main/java/com/example/KingsMen/model/products.java
@@ -1,4 +1,19 @@
 package com.example.KingsMen.model;
 
 public class products {
+    private int product_ID;
+    private String product_title;
+    private String product_imgs;
+    private String product_description;
+    private String product_cat;
+    private int product_price;
+
+    public products(int product_ID, String product_title, String product_imgs, String product_description, String product_cat, int product_price) {
+        this.product_ID = product_ID;
+        this.product_title = product_title;
+        this.product_imgs = product_imgs;
+        this.product_description = product_description;
+        this.product_cat = product_cat;
+        this.product_price = product_price;
+    }
 }
