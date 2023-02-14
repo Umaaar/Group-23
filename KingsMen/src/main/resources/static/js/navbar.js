88d37f1a5a865c540f0b00941134a6c1b7dbf8aa
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2023-02-14 21:32:13 +0000

    Updated Navbar fetch link and deleted duplicate static folder

--- KingsMen/src/main/resources/static/js/navbar.js
+++ KingsMen/src/main/resources/static/js/navbar.js
@@ -1,4 +1,4 @@
-fetch('navbar.html')
+fetch('../../templates/frontend-views/navbar.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
