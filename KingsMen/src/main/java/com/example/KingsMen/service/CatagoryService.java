1c88f12281815c4673914321c7b23366dab33d8e
dduwa <99512283+dduwa@users.noreply.github.com>
2023-02-16 18:31:24 +0000

    category crud

--- KingsMen/src/main/java/com/example/KingsMen/service/CatagoryService.java
+++ KingsMen/src/main/java/com/example/KingsMen/service/CatagoryService.java
@@ -2,25 +2,30 @@ package com.example.KingsMen.service;
 
 import com.example.KingsMen.model.Catagory;
 import com.example.KingsMen.repository.CatagoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 
 @Service
 public class CatagoryService {
+    @Autowired
+    CatagoryRepository catagoryRepository;
 
-    private final CatagoryRepository catagoryRepository;
+    public List<Catagory> getAllCategory() {
+        return catagoryRepository.findAll();
+    }
+
+    public void addCategory(Catagory catagory) {
+        catagoryRepository.save(catagory);
+    }
 
     public CatagoryService(CatagoryRepository catagoryRepository) {
         this.catagoryRepository = catagoryRepository;
     }
-    public void save(Catagory catagory) {
-        catagoryRepository.save(catagory);
-    }
 
-    public List<Catagory> findAll() {
-        return catagoryRepository.findAll();
-    }
+
 
 
 
