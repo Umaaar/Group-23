5036e8e42fcba695db3cf671028f4795a39595f0
dduwa <99512283+dduwa@users.noreply.github.com>
2023-02-16 18:36:54 +0000

    category crud

--- KingsMen/src/main/java/com/example/KingsMen/controller/AdminController.java
+++ KingsMen/src/main/java/com/example/KingsMen/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.example.KingsMen.controller;
 
+import com.example.KingsMen.model.Catagory;
 import com.example.KingsMen.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AdminController {
@@ -23,9 +25,24 @@ public class AdminController {
    }
 
     @GetMapping("/admin/categories/create")
-    public String adminCreateCat(){ 
+    public String postCreateCat(Model model){ 
+        model.addAttribute("category", new Catagory());
      return "/backend-views/category-create";
     }
+
+    @PostMapping("/admin/categories/create")
+    public String adminCreateCat(@ModelAttribute("category") Catagory category, Model model){ 
+        model.addAttribute("category", new Catagory());
+     return "/backend-views/category-create";
+    }
+
+
+
+
+
+
+
+
 
    @GetMapping("/admin/products")
     public String adminProducts(Model model){
