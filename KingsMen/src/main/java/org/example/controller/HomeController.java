d33adadd7cc74cb3ab79cfb8bcc4e90080e47c53
dduwa <99512283+dduwa@users.noreply.github.com>
2023-02-25 23:15:46 +0000

    added nav and footer on frontend pages
    Co-Authored-By: pazara1208 <83435054+pazara1208@users.noreply.github.com>
    Co-Authored-By: Hafeezzzz <116155650+Hafeezzzz@users.noreply.github.com>

--- KingsMen/src/main/java/org/example/controller/HomeController.java
+++ KingsMen/src/main/java/org/example/controller/HomeController.java
@@ -11,30 +11,21 @@ public class HomeController {
         return "/frontend-views/index";
     }
 
-    @GetMapping("/CustomerDashboard")
+    @GetMapping("/customer-dashboard")
     public String dashboard() {
         return "/frontend-views/customer-dashboard";
     }
 
-    @GetMapping("/Contact-us")
+    @GetMapping("/contact-us")
     public String ContactPage() {
         return "/frontend-views/contact-details";
     }
 
-    @GetMapping("/About-us")
+    @GetMapping("/about-us")
     public String AboutPage() {
         return "/frontend-views/about-us";
     }
 
-    @GetMapping("/Login")
-    public String LoginPage() {
-        return "/frontend-views/login";
-    }
-
-    @GetMapping("/Register")
-    public String RegisterPage() {
-        return "/frontend-views/register";
-    }
-
+  
 
 }
