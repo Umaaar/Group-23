3f283ca8d086fde8d0b90b800b2ead439fa6b5b4
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2023-03-04 14:46:11 +0000

    Cart Page, Size can be displayed and Cart Count

--- KingsMen/src/main/java/org/example/controller/CartController.java
+++ KingsMen/src/main/java/org/example/controller/CartController.java
@@ -1,7 +1,7 @@
 package org.example.controller;
 
 import org.example.model.Product;
-
+import org.example.dto.ProductDTO;
 import org.example.global.GlobalData;
 import org.example.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -40,6 +41,16 @@ public class CartController {
         model.addAttribute("total",GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
         return "checkout";
 
+    }
+
+    @PostMapping("/addToCart/{id}")
+    public String dropdown(@PathVariable Long id, ProductDTO dropdown, Model model){
+        Product item = productService.getProductById(id).get();
+        item.setSize(dropdown.getSize());
+        item.setQuantity(dropdown.getQuantity());
+        // model.addAttribute("quantity", dropdown.getQuantity());
+        GlobalData.cart.add(productService.getProductById(id).get());
+        return "redirect:/cart";
     }
 
 }
