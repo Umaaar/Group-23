08985c998b4d6cfb48dff8da9123cf6e42df539b
farandead <92890148+farandead@users.noreply.github.com>
2023-03-24 15:48:08 +0000

    cart

--- KingsMen/src/main/java/org/example/repository/ProductSizeRepository.java
+++ KingsMen/src/main/java/org/example/repository/ProductSizeRepository.java
@@ -1,6 +1,7 @@
 package org.example.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.example.model.ProductSize;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,7 @@ public interface ProductSizeRepository extends JpaRepository<ProductSize, Long> 
 
     List<ProductSize> findAllByProductId(Long id);
     List<ProductSize> findAllBySizeId(Long id);
+    Optional<ProductSize> findByProductIdAndSizeId(long productId, long sizeId);
     
     // @Modifying
     // @Query("UPDATE PRODUCT_SIZE p SET p.QUANTITY = p.QUANTITY - :quantity WHERE p.ID = :productsizeid")
