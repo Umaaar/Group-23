323e1ccd61da6f8234898676442d221404214005
farandead <92890148+farandead@users.noreply.github.com>
2023-03-24 15:48:36 +0000

    cart

--- KingsMen/src/main/java/org/example/repository/SizeRepository.java
+++ KingsMen/src/main/java/org/example/repository/SizeRepository.java
@@ -4,6 +4,9 @@ package org.example.repository;
 import org.example.model.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SizeRepository extends JpaRepository<Size,Long> {
+    Optional<Size> findByName(String name);
   
 }
